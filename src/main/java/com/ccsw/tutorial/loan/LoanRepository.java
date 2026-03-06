8ddd614f20ed117793566caf1c6afc9280ef0f71
Pelayo Alvarez <alvarezrodriguezpelayo@gmail.com>
2026-03-06 11:24:41 +0100

    Soluciono bug entre fechas

--- src/main/java/com/ccsw/tutorial/loan/LoanRepository.java
+++ src/main/java/com/ccsw/tutorial/loan/LoanRepository.java
@@ -9,9 +9,16 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 
 public interface LoanRepository extends CrudRepository<Loan, Long>, JpaSpecificationExecutor<Loan> {
-    @Query("SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END FROM Loan l " + "WHERE l.game.id = :gameId " + "AND l.loanDate <= :loanDate " + "AND l.returnDate >= :loanDate " + "AND (:loanId IS NULL OR l.id <> :loanId)")
-    boolean existsByGameIdAndDates(@Param("gameId") long gameId, @Param("loanDate") LocalDate loanDate, @Param("returnDate") LocalDate returnDate, @Param("loanId") Long loanId);
-
+    @Query("SELECT CASE WHEN COUNT(l) > 0 THEN true ELSE false END FROM Loan l " +
+            "WHERE l.game.id = :gameId " +
+            "AND l.loanDate <= :returnDate " + // <-- CORREGIDO
+            "AND l.returnDate >= :loanDate " +
+            "AND (:loanId IS NULL OR l.id <> :loanId)")
+    boolean existsByGameIdAndDates(
+            @Param("gameId") long gameId,
+            @Param("loanDate") LocalDate loanDate,
+            @Param("returnDate") LocalDate returnDate,
+            @Param("loanId") Long loanId);
     @Query("SELECT COUNT(l) FROM Loan l " + "WHERE l.client.id = :clientId " + "AND l.loanDate <= :returnDate " + "AND l.returnDate >= :loanDate " + "AND (:loanId IS NULL OR l.id <> :loanId)")
     Long countByClientIdAndDates(@Param("clientId") Long clientId, @Param("loanDate") LocalDate loanDate, @Param("returnDate") LocalDate returnDate, @Param("loanId") Long loanId);
 }
